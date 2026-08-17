2083cbda25e2d7d9fb69fded10c293566adc8f9c
FerriolCalvet <ferriolcalvet@gmail.com>
2026-08-17 11:52:45 +0200

    update family size and trinucleotide

--- extended/extended4/compare_trinucleotide_proportions.py
+++ extended/extended4/compare_trinucleotide_proportions.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 
+import sys
 import click
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
+sys.path.append('../../') 
+from consensus_variables import *
 
 
 def plot_trinucleotide_proportions(wgs_counts_file):
@@ -23,7 +27,7 @@ def plot_trinucleotide_proportions(wgs_counts_file):
     counts_all = wgs_counts.copy()
     for cnsq in ["all", "non_protein_affecting", "introns_intergenic", "exons_splice_sites"]:
         try:
-            wgs_counts_cnsq = pd.read_table(f"consensus.{cnsq}.tsv",
+            wgs_counts_cnsq = pd.read_table(f"data/consensus.{cnsq}.tsv",
                                             header = 0,
                                             sep = '\t',
                                             usecols = ["CHROM", "POS", "CONTEXT_MUT", "CONTEXT"]
@@ -42,10 +46,10 @@ def plot_trinucleotide_proportions(wgs_counts_file):
 
     proportions_all_plot = proportions_all.copy()
 
-    fig, axs = plt.subplots(2, 2, figsize=(10, 9))
+    fig, axs = plt.subplots(1, 2, figsize=(4.2, 2))
     axs = axs.flatten()  # Flatten for easy iteration
 
-    for i, cnsq in enumerate(["all", "non_protein_affecting", "introns_intergenic", "exons_splice_sites"]):
+    for i, cnsq in enumerate(["all", "non_protein_affecting"]):
         ax = axs[i]
         try :
             rmse = np.sqrt(((proportions_all_plot["COUNT_WGS"] - proportions_all_plot[f"COUNT_{cnsq}"])**2).mean())
@@ -60,6 +64,7 @@ def plot_trinucleotide_proportions(wgs_counts_file):
                         y=f"COUNT_{cnsq}",
                         hue="CONTEXT",
                         legend=False,
+                        s = 10,
                         ax=ax)
         
         # Identity line (x = y)
@@ -74,61 +79,56 @@ def plot_trinucleotide_proportions(wgs_counts_file):
         # Annotate points with CONTEXT
         for namee, row in proportions_all_plot.iterrows():
             ax.text(row["COUNT_WGS"], row[f"COUNT_{cnsq}"], namee,
-                    fontsize=6, alpha=0.7)
+                    fontsize=3.5, alpha=0.7)
         
         ax.set_xlabel("Proportion WGS")
         ax.set_ylabel(f"Proportion {cnsq}")
         ax.set_title(f"{cnsq} (RMSE: {rmse:.4f})")
 
-    fig.suptitle("Proportions of trinucleotide contexts in WGS vs. consensus panels", fontsize=16)
+    fig.suptitle("Proportions of trinucleotide contexts in WGS vs. consensus panels", fontsize=7)
     plt.tight_layout()
-    plt.savefig("Trinucleotide_content_comparisons.proportions.png", dpi=300)
+    plt.savefig("Trinucleotide_content_comparisons.proportions.pdf", bbox_inches='tight', dpi=300)
     plt.show()
 
 
 
-    ## Count based
-    counts_all_plot = counts_all.copy()
-    fig, axs = plt.subplots(2, 2, figsize=(10, 9))
-    axs = axs.flatten()  # Flatten to make iteration easier
+    # ## Count based
+    # counts_all_plot = counts_all.copy()
+    # fig, axs = plt.subplots(2, 2, figsize=(10, 9))
+    # axs = axs.flatten()  # Flatten to make iteration easier
 
-    for i, cnsq in enumerate(["all", "non_protein_affecting", "introns_intergenic", "exons_splice_sites"]):
-        ax = axs[i]
+    # for i, cnsq in enumerate(["all", "non_protein_affecting", "introns_intergenic", "exons_splice_sites"]):
+    #     ax = axs[i]
 
-        # rmse = np.sqrt(((counts_all_plot["COUNT_WGS"] - counts_all_plot[f"COUNT_{cnsq}"])**2).mean())
-        try:
-            sns.scatterplot(data=counts_all_plot,
-                            x="COUNT_WGS",
-                            y=f"COUNT_{cnsq}",
-                            hue="CONTEXT",
-                            legend=False,
-                            ax=ax)
-        except KeyError:
-            print(f"KeyError: 'COUNT_{cnsq}' not found in counts_all_plot. Skipping scatter plot for this panel.")
-            continue
+    #     # rmse = np.sqrt(((counts_all_plot["COUNT_WGS"] - counts_all_plot[f"COUNT_{cnsq}"])**2).mean())
+    #     try:
+    #         sns.scatterplot(data=counts_all_plot,
+    #                         x="COUNT_WGS",
+    #                         y=f"COUNT_{cnsq}",
+    #                         hue="CONTEXT",
+    #                         legend=False,
+    #                         ax=ax)
+    #     except KeyError:
+    #         print(f"KeyError: 'COUNT_{cnsq}' not found in counts_all_plot. Skipping scatter plot for this panel.")
+    #         continue
     
-        # Annotate points with CONTEXT
-        for namee, row in counts_all_plot.iterrows():
-            ax.text(row["COUNT_WGS"], row[f"COUNT_{cnsq}"], namee,
-                    fontsize=6, alpha=0.7)
+    #     # Annotate points with CONTEXT
+    #     for namee, row in counts_all_plot.iterrows():
+    #         ax.text(row["COUNT_WGS"], row[f"COUNT_{cnsq}"], namee,
+    #                 fontsize=6, alpha=0.7)
 
-        ax.set_xlabel("Counts WGS")
-        ax.set_ylabel(f"Counts {cnsq}")
-        ax.set_title(f"{cnsq}")
-    fig.suptitle("Counts of trinucleotide contexts in WGS vs. consensus panels", fontsize=16)
-    plt.tight_layout()
-    plt.savefig("Trinucleotide_content_comparisons.counts.png", dpi=300)
-    plt.show()
+    #     ax.set_xlabel("Counts WGS")
+    #     ax.set_ylabel(f"Counts {cnsq}")
+    #     ax.set_title(f"{cnsq}")
+    # fig.suptitle("Counts of trinucleotide contexts in WGS vs. consensus panels", fontsize=16)
+    # plt.tight_layout()
+    # plt.savefig("Trinucleotide_content_comparisons.counts.pdf", dpi=300)
+    # plt.show()
 
 
-@click.command()
-@click.option('--wgs-trinucleotide', type=click.Path(exists=True), help='Input trinucleotide counts file for WGS')
-def main(wgs_trinucleotide):
+def main():
     click.echo("Comparing the trinucleotide proportions...")
-    try:
-        plot_trinucleotide_proportions(wgs_trinucleotide)
-    except Exception as e:
-        click.echo(f"Error occurred: {e}")
+    plot_trinucleotide_proportions("data/trinuc_counts.homo_sapiens.tsv")
 
 if __name__ == '__main__':
     main()
